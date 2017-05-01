a6cc602c0ca32d11ecd05a22f7c510506c323b3b
ParkerWSmith <smithparker0@gmail.com>
2017-05-01 04:33:58 -0500

    Source stuff added + temperature is still off

--- WeatherApp/WeatherApp/Core.cs
+++ WeatherApp/WeatherApp/Core.cs
@@ -7,10 +7,10 @@ namespace WeatherApp
     {
         public static async Task<Weather> GetWeather(string zipCode)
         {
+            //See https://msdn.microsoft.com/en-us/library/dn879698.aspx#Android for information on how this works
             string key = "b0f6583a877d6f9d042e76e575470acb";
             string queryString = "http://api.openweathermap.org/data/2.5/weather?zip="
                 + zipCode + ",&appid=" + key;
-
 
             var results = await DataService.GetDataFromService(queryString).ConfigureAwait(false);
 
@@ -23,11 +23,11 @@ namespace WeatherApp
                 weather.Humidity = (string)results["main"]["humidity"] + " %";
                 weather.Visibility = (string)results["weather"][0]["main"];
 
-                DateTime time = DateTime.Now;
+                DateTime time = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
                 DateTime sunrise = time.AddSeconds((double)results["sys"]["sunrise"]);
                 DateTime sunset = time.AddSeconds((double)results["sys"]["sunset"]);
-                weather.Sunrise = sunrise.ToString() + " CST";
-                weather.Sunset = sunset.ToString() + " CST";
+                weather.Sunrise = sunrise.ToString() + " UTC";
+                weather.Sunset = sunset.ToString() + " UTC";
                 return weather;
             }
             else
