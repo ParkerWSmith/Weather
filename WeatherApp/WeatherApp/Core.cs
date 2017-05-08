f3e536b09caa8ec007ea4263c8311a263b867d2f
ParkerWSmith <smithparker0@gmail.com>
2017-05-07 22:52:43 -0500

    Time Works Data is off

--- WeatherApp/WeatherApp/Core.cs
+++ WeatherApp/WeatherApp/Core.cs
@@ -23,11 +23,11 @@ namespace WeatherApp
                 weather.Humidity = (string)results["main"]["humidity"] + " %";
                 weather.Visibility = (string)results["weather"][0]["main"];
 
-                DateTime time = DateTime.Now; 
+                DateTime time = new System.DateTime(1970, 1, 1, 19, 0, 0, 0);
                 DateTime sunrise = time.AddSeconds((double)results["sys"]["sunrise"]);
                 DateTime sunset = time.AddSeconds((double)results["sys"]["sunset"]);
-                weather.Sunrise = sunrise.ToString() + " CST";
-                weather.Sunset = sunset.ToString() + " CST";
+                weather.Sunrise = sunrise.ToString() + " CDT";
+                weather.Sunset = sunset.ToString() + " CDT";
                 return weather;
             }
             else
