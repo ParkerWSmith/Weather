15760f3cec4b0c8721c7671c6248f4808d3516b2
ParkerWSmith <smithparker0@gmail.com>
2017-05-15 18:58:48 -0500

    Finished

--- WeatherApp/WeatherApp/Core.cs
+++ WeatherApp/WeatherApp/Core.cs
@@ -7,6 +7,7 @@ namespace WeatherApp
     {
         public static async Task<Weather> GetWeather(string zipCode)
         {
+            //Info for open weather
             string key = "b0f6583a877d6f9d042e76e575470acb";
             string queryString = "http://api.openweathermap.org/data/2.5/weather?zip="
                 + zipCode + "&units=imperial&appid=" + key;
@@ -15,6 +16,7 @@ namespace WeatherApp
 
             if (results["weather"] != null)
             {
+                //Differnt weather information
                 Weather weather = new Weather();
                 weather.Title = (string)results["name"];
                 weather.Temperature = (string)results["main"]["temp"] + " F";
@@ -22,6 +24,8 @@ namespace WeatherApp
                 weather.Humidity = (string)results["main"]["humidity"] + " %";
                 weather.Visibility = (string)results["weather"][0]["main"];
 
+
+                //Time info for sunrise/sunset
                 DateTime time = new System.DateTime(1970, 1, 1, 19, 0, 0, 0);
                 DateTime sunrise = time.AddSeconds((double)results["sys"]["sunrise"]);
                 DateTime sunset = time.AddSeconds((double)results["sys"]["sunset"]);
