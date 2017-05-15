58c5531d44d88dd43301684c8ce4b49e9eaeea86
ParkerWSmith <smithparker0@gmail.com>
2017-05-15 06:20:02 -0500

    Date fixed

--- WeatherApp/WeatherApp/Core.cs
+++ WeatherApp/WeatherApp/Core.cs
@@ -7,7 +7,6 @@ namespace WeatherApp
     {
         public static async Task<Weather> GetWeather(string zipCode)
         {
-            //See https://msdn.microsoft.com/en-us/library/dn879698.aspx#Android for information on how this works
             string key = "b0f6583a877d6f9d042e76e575470acb";
             string queryString = "http://api.openweathermap.org/data/2.5/weather?zip="
                 + zipCode + "&units=imperial&appid=" + key;
